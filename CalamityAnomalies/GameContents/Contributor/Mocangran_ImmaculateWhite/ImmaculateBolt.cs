794463991ede2b3d98bd7f988a8f85283c95142f
mocangran <284992564@qq.com>
2026-05-21 09:09:48 +0800

    纯白月前平衡，新增弹幕特性，右键攻击

--- CalamityAnomalies/GameContents/Contributor/Mocangran_ImmaculateWhite/ImmaculateBolt.cs
+++ CalamityAnomalies/GameContents/Contributor/Mocangran_ImmaculateWhite/ImmaculateBolt.cs
@@ -1,10 +1,14 @@
 ﻿// Developed by ColdsUx
 
+using Terraria.GameContent.ItemDropRules;
+
 namespace CalamityAnomalies.GameContents.Contributor.Mocangran_ImmaculateWhite;
 
 public sealed class ImmaculateBolt : CAModProjectile
 {
-    public const int LifeTime = 450;
+    //似乎是存了一个最大timelife，但是好像有些错位
+    //方便热重载暂时从常量改成字段，不过这玩意真有必要做个常量吗
+    public int LifeTime = 2400;
 
     public NPC Target
     {
@@ -17,7 +21,12 @@ public sealed class ImmaculateBolt : CAModProjectile
         set => Projectile.ai[0] = value?.whoAmI ?? -1;
     }
 
-    public bool IsBigProjectile => Projectile.ai[1] == 0f;
+    public bool IsNotSmallProjectile => Projectile.ai[1] != 1f;
+
+    public float NPCMaxLife { get; set; }//存一下npc的生命值，应该？不需要同步吧
+    public bool IsFirstHit { get; set; } = true;//第一次击中npc，也许（？）可以模拟一下穿透数。
+    public int ItemPentrate { get; set; } = 1;//存储穿透数
+    public int PentrateExtraDamage { get; set; }//穿透数额外伤害，超过原本伤害上限的部分会以这种形式存在，下一次击中时会加到伤害里
 
     public override string LocalizationCategory => "GameContents.Contributor";
 
@@ -35,11 +44,12 @@ public sealed class ImmaculateBolt : CAModProjectile
         Projectile.height = 30;
         Projectile.alpha = 255;
         Projectile.friendly = true;
-        Projectile.timeLeft = 300;
+        Projectile.timeLeft = 2400;
         Projectile.tileCollide = false;
+        Projectile.DamageType = DamageClass.Magic;
         Projectile.ignoreWater = true;
         Projectile.extraUpdates = 4;
-        Projectile.penetrate = 3;
+        Projectile.penetrate = -1;
         Projectile.usesLocalNPCImmunity = true;
         Projectile.localNPCHitCooldown = 50;
         Projectile.ArmorPenetration = 200;
@@ -47,28 +57,38 @@ public sealed class ImmaculateBolt : CAModProjectile
         Target = NPC.DummyNPC;
     }
 
+    /*public override void OnSpawn(IEntitySource source)
+    {
+        if (source is EntitySource_Parent parent && parent.Entity is Projectile projectile && projectile.ModProjectile is ImmaculateWhiteBow immaculateWhiteBow)
+        {
+            ItemPentrate = immaculateWhiteBow.itemPentrate;
+        }
+    }*/
     public override void AI()
     {
         Timer1++;
 
-        float linearAccelerationLimit = IsBigProjectile ? 10f : 4f;
+        float linearAccelerationLimit = IsNotSmallProjectile ? 10f : 4f;
         if (Projectile.velocity.Modulus <= linearAccelerationLimit)
         {
-            float acceleration = IsBigProjectile ? 0.2f : 0.5f;
+            float acceleration = IsNotSmallProjectile ? 0.2f : 0.5f;
             Projectile.velocity.Modulus += acceleration;
         }
         else
-            Projectile.velocity *= 1.001f;
+            Projectile.velocity *= 1.005f;
 
-        if (Timer1 > 40)
+        int starTime = 10;
+        int maxLifeTime = 1200;
+        if (Timer1 > starTime)
         {
-            float homingRatio = Utils.Remap(Timer1, 40, LifeTime, 0.2f, 0.4f);
-            if (Target is null || !Target.CanBeChasedBy() || !Projectile.HomeIn(Target, HomingAlgorithm.SmoothStep, homingRatio, 8000f, MathHelper.TwoPi, false))
-                Target = TOKinematicUtils.GetNPCTarget(Projectile.Center, 8000f, ignoreTiles: true);
+            float homingRatio = Utils.Remap(Timer1, starTime, maxLifeTime, 0.2f, 0.4f);
+            if (Target is null || !Target.CanBeChasedBy() || !Projectile.HomeIn(Target, HomingAlgorithm.SmootherStep, homingRatio, 8000f, MathHelper.TwoPi, false))
+                Target = TOKinematicUtils.GetNPCTarget(Projectile.Center, 8000f, ignoreTiles: true, bossPriority: Projectile.ai[2] >= 2f);
         }
 
         Projectile.VelocityToRotation(MathHelper.PiOver2);
     }
+
 
     public override Color? GetAlpha(Color lightColor) => Color.White * Projectile.Opacity;
 
@@ -136,22 +156,22 @@ public sealed class ImmaculateBolt : CAModProjectile
         Vector2 centerScreenPos = Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY);
         Main.EntitySpriteDraw(projectileTexture, centerScreenPos, sourceRectangle, baseColor, Projectile.rotation, origin, Projectile.scale * 0.9f, spriteEffects);
 
-        // Sharp tears effect overlay
+        // Sharp tears effect overlay。七彩矢额外闪烁特效：其实就是把七彩矢贴图拉长然后横着一个竖着一个
         Texture2D sharpTearsTexture = TextureAssets.Extra[ExtrasID.SharpTears].Value;
         Color tearsBaseColor = baseColor;
         Vector2 tearsOrigin = sharpTearsTexture.Size() / 2f;
         Color tearsDimColor = baseColor * 0.5f;
 
-        float glowFactor = Utils.GetLerpValue(15f, 30f, Projectile.timeLeft, clamped: true)
-            * Utils.GetLerpValue(LifeTime, LifeTime - 60, Projectile.timeLeft, clamped: true)
-            * (1f + 0.2f * (float)Math.Cos(Main.GlobalTimeWrappedHourly % 30f / 0.5f * ((float)Math.PI * 2f) * 3f))
+        float glowFactor = Utils.GetLerpValue(15f, 30f, Projectile.timeLeft, clamped: true)//消失时的停止闪烁，但是对这个弹幕来说没太多必要（
+            * Utils.GetLerpValue(LifeTime, LifeTime - 10, Projectile.timeLeft, clamped: true)//刚生成时逐渐开始闪烁.
+            * (1f + 0.2f * (float)Math.Cos(Main.GlobalTimeWrappedHourly % 30f / 0.5f * ((float)Math.PI * 2f) * 3f))//这里是控制大小闪烁
             * 0.8f * Projectile.scale;
 
         Vector2 glowScaleVertical = new Vector2(0.5f, 5f) * glowFactor;
         Vector2 glowScaleHorizontal = new Vector2(0.5f, 2f) * glowFactor;
         tearsBaseColor *= glowFactor;
         tearsDimColor *= glowFactor;
-
+        
         // Offset multiplier is zero, so position stays the same
         int glowOffsetMultiplier = 0;
         Vector2 glowPosition = centerScreenPos + Projectile.velocity.SafeNormalize(Vector2.Zero) * MathHelper.Lerp(0.5f, 1f, Projectile.localAI[0] / 60f) * glowOffsetMultiplier;
@@ -173,11 +193,16 @@ public sealed class ImmaculateBolt : CAModProjectile
         return false;
     }
 
-    public override bool? CanHitNPC(NPC target) => !target.friendly && (IsBigProjectile || Timer1 > 40);
+    public override bool? CanHitNPC(NPC target) => !target.friendly && (IsNotSmallProjectile || Timer1 > 40);
+
+    public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+    {
+        NPCMaxLife += target.life;
+    }
 
     public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
     {
-        if (Projectile.ai[2] == 1f)
+        if (Projectile.ai[2] >= 1f)
         {
             for (int i = 0; i < 2; i++)
             {
@@ -185,9 +210,48 @@ public sealed class ImmaculateBolt : CAModProjectile
                 {
                     p.scale /= 2f;
                     p.ai[1] = 1f;
+                    if (p.ModProjectile is ImmaculateBolt bow)
+                    {
+                        bow.ItemPentrate = ItemPentrate;
+                    }
                 });
             }
         }
+        //改了一堆数值之后发现穿透数多了之后好乱，所以做成了下面这样的效果
+        //击中npc后如果伤害溢出了，能够以溢出的伤害继续攻击
+
+        //我写的代码你觉得哪里有问题的话改改就行，总之应该是没bug（
+
+        //此处模拟穿透数。
+        int pentrate = 0 + (IsFirstHit && IsNotSmallProjectile ? ItemPentrate : 0);
+        int extraDamage = Projectile.damage * pentrate;
+        if (hit.SourceDamage + extraDamage >= MathHelper.Clamp(NPCMaxLife, 0f, hit.SourceDamage))
+        {
+            float newDamage = Math.Max(0f, hit.SourceDamage + extraDamage - MathHelper.Clamp(NPCMaxLife, 0f, hit.SourceDamage));
+            NPCMaxLife = 0;
+            newDamage += PentrateExtraDamage;
+            PentrateExtraDamage = 0;
+            if (newDamage > 1)
+            {
+                if (newDamage > Projectile.damage)
+                {
+                    PentrateExtraDamage += (int)newDamage - Projectile.damage;
+                    newDamage = Projectile.damage;
+                }
+                Projectile.damage = (int)newDamage;
+                //然后我是不是应该动一下弹幕存在时长
+                Projectile.timeLeft = 1200 - 10;
+            }
+            else
+            {
+                Projectile.Kill();
+            }
+        }
+        else
+        {
+            Projectile.Kill();
+        }
+        IsFirstHit = false;
     }
 
     public override void OnKill(int timeLeft)
--- CalamityAnomalies/GameContents/Contributor/Mocangran_ImmaculateWhite/ImmaculateWhiteBow.cs
+++ CalamityAnomalies/GameContents/Contributor/Mocangran_ImmaculateWhite/ImmaculateWhiteBow.cs
@@ -6,6 +6,11 @@ namespace CalamityAnomalies.GameContents.Contributor.Mocangran_ImmaculateWhite;
 
 public sealed class ImmaculateWhiteBow : CAModProjectile
 {
+    public int itemPentrate = 1;//穿透数存储中间站
+
+    public float ShootSpeed = 20f;//实际武器攻速
+
+    public int CirtLimit => (int)ShootSpeed;
     public override LocalizedText DisplayName => TOLocalizationUtils.GetItemName<ImmaculateWhite>();
     public override string Texture => "CalamityAnomalies/GameContents/Contributor/Mocangran_ImmaculateWhite/ImmaculateWhite";
 
@@ -16,15 +21,39 @@ public sealed class ImmaculateWhiteBow : CAModProjectile
         Projectile.friendly = true;
         Projectile.penetrate = -1;
         Projectile.tileCollide = false;
-        Projectile.DamageType = DamageClass.Ranged;
+        Projectile.DamageType = DamageClass.Magic;
         Projectile.ignoreWater = true;
     }
 
+    /*public override void OnSpawn(IEntitySource source)
+    {
+        if(source is EntitySource_Parent parent && parent.Entity is Item item && item.ModItem is ImmaculateWhite immaculateWhite)
+        {
+            itemPentrate = immaculateWhite.itemPentrate;
+        }
+    }*/
+
     public override void AI()
     {
+        //说实话手持弹幕来做这个武器感觉有点多余（？
+        //这就导致了词缀的攻击速度加不到这里，不过以后再说吧（
+        ShootSpeed = 20f;
+        if (NPC.downedBoss2)//世吞克脑
+            ShootSpeed -= 1f;
+        if (NPC.downedBoss3)//骷髅王
+            ShootSpeed -= 1f;
+        if (NPC.downedGolemBoss)//石巨人
+            ShootSpeed -= 1f;
+        if (DownedBossSystem_Bridge.downedCeaselessVoid 
+            && DownedBossSystem_Bridge.downedSignus 
+            && DownedBossSystem_Bridge.downedStormWeaver)//无尽虚空、西格纳斯，风编
+            ShootSpeed -= 1f;
+        if (DownedBossSystem_Bridge.downedYharon)//犽戎
+            ShootSpeed -= 1f;
+
         Lighting.AddLight(Projectile.Center, 1f, 1f, 1f);
         Player player = Projectile.Owner;
-        bool canUseItem = !player.CantUseHoldout() && player.HasAmmo(player.HeldItem);
+        bool canUseItem = (!player.CantUseHoldout() || !CantUseRightHoldout(player)) && player.HasAmmo(player.HeldItem);
         Vector2 actualPlayerPosition = player.RotatedRelativePoint(player.MountedCenter, true);
 
         if (Projectile.IsOnOwnerClient)
@@ -53,7 +82,7 @@ public sealed class ImmaculateWhiteBow : CAModProjectile
                 player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, armRotation + 0f * Projectile.spriteDirection);
                 player.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.Full, armRotation + 0f * Projectile.spriteDirection);
 
-                if (Timer1 % 20 == 0)
+                if (Timer1 % ShootSpeed == 0)
                 {
                     player.PickAmmo(player.HeldItem, out int type, out float speed, out _, out float knockback, out _);
                     knockback = player.GetWeaponKnockback(player.HeldItem, knockback);
@@ -61,25 +90,47 @@ public sealed class ImmaculateWhiteBow : CAModProjectile
                     Vector2 originalProjectileSpawnCenter = Projectile.Center + new PolarVector2(20f, originalRotation);
 
                     float angleOffset = Main.rand.NextFloat(-0.05f, 0.05f);
-                    float angle = originalRotation + angleOffset;
+                    float angle = originalRotation;// + angleOffset;
                     Vector2 projectileSpawnCenter = originalProjectileSpawnCenter + new PolarVector2(10f, angle);
-                    Projectile.NewProjectileAction<ImmaculateBolt>(Projectile.GetSource_FromAI(), projectileSpawnCenter, new PolarVector2(Main.rand.NextFloat(2f, 2.5f), angle), Projectile.damage, knockback, player.whoAmI, p =>
+                    if (Projectile.ai[2] >= 2f && player.altFunctionUse == 2)
                     {
-                        if (Projectile.ai[2] == 1f)
-                            p.ai[2] = 1f;
-                    });
+                        Projectile.NewProjectileAction<InfiniteImmaculateBolt>(Projectile.GetSource_FromAI(), projectileSpawnCenter, new PolarVector2(Main.rand.NextFloat(2f, 2.5f), angle), Projectile.damage / 2, knockback, player.whoAmI);
+                    }
+                    else
+                    {
+                        Projectile.NewProjectileAction<ImmaculateBolt>(Projectile.GetSource_FromAI(), projectileSpawnCenter, new PolarVector2(Main.rand.NextFloat(2f, 2.5f), angle), Projectile.damage, knockback, player.whoAmI, p =>
+                        {
+                            if (Projectile.ai[2] >= 1f)
+                                p.ai[2] = 1f;
+                            if (p.ModProjectile is ImmaculateBolt bolt)
+                            {
+                                bolt.ItemPentrate = itemPentrate;
+                            }
+                        });
+                    }
                 }
 
                 Timer1++;
             }
             else
             {
-                player.Anomaly.ImmaculateWhite_Timer = 90;
-                Projectile.Kill();
+                player.Anomaly.ImmaculateWhite_Timer = Main.zenithWorld ? 0 : CirtLimit;
+                //Projectile.Kill();
             }
         }
     }
 
     public override bool? CanHitNPC(NPC target) => false;
     public override bool CanHitPvp(Player target) => false;
+
+    //不知道为啥右键使用没法触发channel，在这写一个额外的。
+    public static bool CantUseRightHoldout(Player player, bool needsToHold = true)
+    {
+        if (player != null && player.active && !player.dead && !(!Main.mouseRight && needsToHold) && !player.CCed)
+        {
+            return player.noItems;
+        }
+
+        return true;
+    }
 }
